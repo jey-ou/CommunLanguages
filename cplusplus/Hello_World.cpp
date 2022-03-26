93998259978b55c74e5ca765e86d32b7611c5c0a
jey-ou <83501213+jey-ou@users.noreply.github.com>
2022-03-26 05:49:57 +0100

    Create Hello_World.cpp

---
+++ cplusplus/Hello_World.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // preprocessor command - header file
+using namespace std; // where to find the headerfiles in the absence of an explicit designation
+
+int manin(){
+  cout << "Hello World";
+  
+  return 0;
+}
